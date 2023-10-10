e101cf74d7c03dd2cfdacdca5fed1c69a05b5add
João Pedro Lipinski Ferreira <joao_pl_ferreira@estudante.sesisenai.org.br>
2023-10-10 16:25:22 -0300

    matrizAtv01

--- src/matriz/matriz.java
+++ src/matriz/matriz.java
@@ -1,10 +1,22 @@
 package matriz;
+import java.util.Scanner;
 
 public class matriz {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner (System.in);
+		
+		int matriz[][] = new int [4][4];
+		
+		for (int linha = 0; linha < matriz.length; linha++) {
+			for (int coluna = 0; coluna < matriz.length; coluna++) {
+				System.out.println("Digite valor: ");
+				matriz[linha][coluna] = sc.nextInt();
+			}
+			
+		}
+		
 	}
 
 }
